7d0a8609d0468a5c69ea1813a06259a397445409
C B <cbandara1@gmail.com>
2023-03-31 22:25:48 -0500

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "nirvanalabs"
 
     workspaces {
-      name = "learn-terraform-cloud"
+      name = "ckb-test"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
